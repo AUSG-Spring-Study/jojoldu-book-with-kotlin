562bd1037a6c15b65bc89483c58499a40d915198
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-08-10 21:12:31 +0900

    "/hello/dto" 구현 및 테스트 / 2장 끝

--- src/main/kotlin/me/ausg/purplebookwithkotlin/web/HelloController.kt
+++ src/main/kotlin/me/ausg/purplebookwithkotlin/web/HelloController.kt
@@ -1,9 +1,16 @@
 package me.ausg.purplebookwithkotlin.web
 
+import me.ausg.purplebookwithkotlin.web.dto.HelloResponseDto
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
-    @GetMapping("/hello") fun hello() : String = "hello"
+    @GetMapping("/hello") fun hello(): String = "hello"
+
+    @GetMapping("/hello/dto") fun helloDto(
+        @RequestParam("name") name: String,
+        @RequestParam("amount") amount: Int,
+    ): HelloResponseDto = HelloResponseDto(name, amount)
 }
--- src/test/kotlin/me/ausg/purplebookwithkotlin/web/HelloControllerTest.kt
+++ src/test/kotlin/me/ausg/purplebookwithkotlin/web/HelloControllerTest.kt
@@ -1,12 +1,13 @@
 package me.ausg.purplebookwithkotlin.web
 
+import me.ausg.purplebookwithkotlin.web.dto.HelloResponseDto
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(controllers = [HelloController::class])
 internal class HelloControllerTest {
@@ -16,14 +17,33 @@ internal class HelloControllerTest {
     @Test
     fun hello가_리턴된다() {
         // given
-        val to = get("/hello")
+        val url = "/hello"
         val expectedContent = "hello";
-        val expectedStatus = status().isOk
 
         // when then
-        mvc.perform(to)
-            .andExpect(expectedStatus)
+        mvc.perform(get(url))
+            .andExpect { status().isOk }
             .andExpect { content().string(expectedContent) }
+    }
+
+    @Test
+    fun HelloResponseDto가_리턴된다() {
+        // given
+        val url = "/hello/dto"
+        val name = "name"
+        val amount = 1000
+
+        // when
+        mvc.perform(
+            get(url)
+                .param("name", name)
+                .param("amount", amount.toString())
+        )
+            .andExpect { status().isOk }
+            .andExpect { jsonPath("$.name", `is`(name)) }
+            .andExpect { jsonPath("$.amount", `is`(amount)) }
+
+        // given
     }
 
 }
